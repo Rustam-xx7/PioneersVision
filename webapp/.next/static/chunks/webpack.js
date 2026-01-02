a9bce215bc6854ba389d606b40facced3cf8faec
Rustam-xx7 <chakrabortyrustam@gmail.com>
2026-01-02 11:12:16 +0530

    change some versions in requirements.txt

--- webapp/.next/static/chunks/webpack.js
+++ webapp/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff1d56fafa5c1c38"; }
+/******/ 		__webpack_require__.h = function() { return "b6106f8ac03ebe80"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- webapp/.next/static/webpack/webpack.1d2448163a6151c0.hot-update.js
+++ webapp/.next/static/webpack/webpack.1d2448163a6151c0.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ff1d56fafa5c1c38"; }
+/******/ 	__webpack_require__.h = function() { return "b6106f8ac03ebe80"; }
 /******/ }();
 /******/ 
 /******/ }
